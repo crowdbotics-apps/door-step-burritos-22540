9ef829ecb04046d2a6336d2b9455039bf5d486f0
Crowdbotics <team@crowdbotics.com>
2020-11-11 21:58:33 +0000

    Initial commit.

---
+++ backend/door_step_burritos_22540/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for door_step_burritos_22540 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'door_step_burritos_22540.settings')
+
+application = get_wsgi_application()
