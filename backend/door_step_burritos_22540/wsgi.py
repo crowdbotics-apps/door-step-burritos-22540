941efc0d5a1836958a837c33871f8b94f57d0c9a
Crowdbotics <team@crowdbotics.com>
2020-11-11 22:00:08 +0000

    Update models

--- backend/door_step_burritos_22540/wsgi.py
+++ backend/door_step_burritos_22540/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'door_step_burritos_22540.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "door_step_burritos_22540.settings")
 
 application = get_wsgi_application()
--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'door_step_burritos_22540.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "door_step_burritos_22540.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +17,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
